d6b5dcfdee9192b5df7d30550347e5acdc39eb87
Juanfu224 <88267143+Juanfu224@users.noreply.github.com>
2025-02-02 02:40:49 +0100

    Delete tools directory
    Signed-off-by: Juanfu224 <88267143+Juanfu224@users.noreply.github.com>

--- tools/bspwm/battery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$(acpi | awk '{print $3}' | tr -d ',')" != "Discharging" ]; then
-
-	echo -e "%{F#27FF00} %{F#e2ee6a}$(acpi | awk '{print $4}' | tr -d ',')%{u-}"
-else
-	echo -e "%{F#FF0000} %{F#e2ee6a}$(acpi | awk '{print $4}' | tr -d ',')%{u-}"
-fi
--- tools/bspwm/ethernet_status.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig wlp1s0 | grep "inet " | awk '{print $2}')%{u-}"
